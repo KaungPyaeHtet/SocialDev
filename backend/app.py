ad7e5c300f15f2876c7e5b73cee5f2929298cbfa
Kaung Pyae Htet <kaungpyaehtet2007@gmail.com>
2025-08-12 00:23:57 +0630

    added user endpoint api

--- backend/app.py
+++ backend/app.py
@@ -10,4 +10,10 @@ def index():
     return json.dumps({'name': 'Kaung Pyae Htet',
                        'email': 'alice@outlook.com'})
 
+@app.route("/users/<user>")
+def show_user_profile(username):
+    return json.dumps({
+        "name":"test"
+    })
+
 app.run()
