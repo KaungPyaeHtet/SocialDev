5c9efef4da0aa8998d284fbd387f30c5e2e27af3
Jade <nyanminhtet2304@gmail.com>
2025-08-14 14:26:30 +0630

    Updated auth.py

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -2,14 +2,14 @@ from functools import wraps
 from flask import request, jsonify, Blueprint, url_for, g, redirect
 from werkzeug.security import check_password_hash, generate_password_hash
 
-auth_bp = Blueprint("login", __name__)
+auth_bp = Blueprint("auth", __name__)
 
 
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if g.user is None:
-            return redirect("login", next=request.url)
+            return redirect(url_for("login", next=request.url))
         return f(*args, **kwargs)
 
     return decorated_function
