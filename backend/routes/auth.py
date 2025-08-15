5a614bd0036ac21b7f21c7fb6de513b28df28013
Jade <nyanminhtet2304@gmail.com>
2025-08-15 23:30:38 +0630

    Added Register Function

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,8 +1,40 @@
+import sqlite3
+
 from functools import wraps
 from flask import request, jsonify, Blueprint, url_for, g, redirect
 from werkzeug.security import check_password_hash, generate_password_hash
 
 auth_bp = Blueprint("auth", __name__)
+
+
+@auth_bp.route("/register", methods=["POST"])
+def register():
+    user_data = request.json
+    username, email, password = (
+        user_data["username"],
+        user_data["email"],
+        user_data["password"],
+    )
+
+    try:
+        with sqlite3.connect("users.db") as conn:
+            db = conn.cursor()
+            hashed_password = generate_password_hash(password)
+
+            db.execute(
+                "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+                (
+                    username,
+                    email,
+                    hashed_password,
+                ),
+            )
+            conn.commit()
+
+    except sqlite3.Error:
+        return jsonify("Username already exists."), 422
+
+    return jsonify(user_data)
 
 
 def login_required(f):
@@ -21,13 +53,8 @@ def login():
     return jsonify(data)
 
 
-@auth_bp.route("/register", methods=["POST"])
-def register():
-    data = request.json
-    return jsonify(data)
-
-
-# @login_required
-# def logout():
+@login_required
+def logout():
+    return None
 
 # REST API, JWT, sqlite3 schema
