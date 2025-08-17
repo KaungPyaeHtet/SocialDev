6d2d4075d160f5a7fc8074681a3c7f4abd756452
Jade <nyanminhtet2304@gmail.com>
2025-08-17 13:05:10 +0630

    Added Input Validation Func

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 from functools import wraps
+from operator import itemgetter
 from flask import request, jsonify, Blueprint, url_for, g, redirect
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -10,16 +11,30 @@ auth_bp = Blueprint("auth", __name__)
 @auth_bp.route("/register", methods=["POST"])
 def register():
     user_data = request.json
-    username, email, password = (
-        user_data["username"],
-        user_data["email"],
-        user_data["password"],
-    )
+
+    # Checks if all fields exist
+    for field in user_data:
+        user_data[field] = user_data[field].strip()
+        if not user_data[field] or user_data[field] == "":
+            return (
+                jsonify(
+                    {
+                        "error": "Bad Request",
+                        "message": f"The '{field}' field is required and cannot be empty.",
+                    }
+                ),
+                400,
+            )
+
+        else:
+            username, email, password = itemgetter("username", "email", "password")(
+                user_data
+            )
 
     try:
         with sqlite3.connect("users.db") as conn:
             db = conn.cursor()
-            hashed_password = generate_password_hash(password, method='pbkdf2:sha256:150000')
+            hashed_password = generate_password_hash(password)
 
             db.execute(
                 "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
@@ -34,7 +49,7 @@ def register():
     except sqlite3.Error:
         return jsonify("Username already exists."), 422
 
-    return jsonify("Welcome " + username)
+    return jsonify({"status": "success", "username": username})
 
 
 def login_required(f):
@@ -42,7 +57,6 @@ def login_required(f):
     def decorated_function(*args, **kwargs):
         if g.user is None:
             return redirect(url_for("login", next=request.url))
-            
         return f(*args, **kwargs)
 
     return decorated_function
@@ -50,12 +64,17 @@ def login_required(f):
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
-    data = request.json
-    return jsonify(data)
+    # Check if users exist in the database
+    # Return JWT with cookies
+    # Make users don't have to sign in frequent.
+    ...
 
 
 @login_required
-def logout():
-    return None
+def logout(): ...
+
+
+# Don't know
+
 
 # REST API, JWT, sqlite3 schema
