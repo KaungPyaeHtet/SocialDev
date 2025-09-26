7fb50559e07e64f6810ed0eed81665c5c157e5a3
Jade <nyanminhtet2304@gmail.com>
2025-09-26 22:01:49 +0630

    Database refresh

--- backend/app/schema.sql
+++ backend/app/schema.sql
@@ -13,29 +13,3 @@ CREATE TABLE
         content TEXT NOT NULL,
         timestamp INTEGER NOT NULL DEFAULT (unixepoch ())
     );
-
--- CREATE TABLE
---     chats (
---         id INTEGER PRIMARY KEY AUTOINCREMENT,
---         chat_name TEXT NOT NULL,
---         is_public BOOLEAN NOT NULL DEFAULT 1,
---         created_at INTEGER NOT NULL DEFAULT (unixepoch ())
---     );
--- CREATE TABLE
---     participants (
---         user_id INTEGER NOT NULL,
---         chat_id INTEGER NOT NULL,
---         PRIMARY KEY (user_id, chat_id),
---         FOREIGN KEY (user_id) REFERENCES users (id),
---         FOREIGN KEY (chat_id) REFERENCES chats (id)
---     );
--- CREATE TABLE
---     messages (
---         id INTEGER PRIMARY KEY AUTOINCREMENT,
---         chat_id INTEGER NOT NULL,
---         sender_id INTEGER NOT NULL,
---         content TEXT NOT NULL,
---         timestamp INTEGER NOT NULL DEFAULT (unixepoch ()),
---         FOREIGN KEY (chat_id) REFERENCES chats (id),
---         FOREIGN KEY (sender_id) REFERENCES users (id)
---     );
